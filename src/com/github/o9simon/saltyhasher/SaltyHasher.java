3e73f29a84dc7c38de1ba2f4ae54b02109612d04
o9simon <simonricgir@gmail.com>
2017-04-17 07:07:24 -0400

    Remove main and fix indents

--- src/com/github/o9simon/saltyhasher/SaltyHasher.java
+++ src/com/github/o9simon/saltyhasher/SaltyHasher.java
@@ -8,11 +8,6 @@ public class SaltyHasher {
 	public static final int INTERLACE_0 = 3;
 	public static final int INTERLACE_1 = 4;
 	
-	public static void main(String[] arg) {
-		SaltyHasher hasher = new SaltyHasher();
-		System.out.println(hasher.sha1("test", "salt", AFTER));
-	}
-	
 	public String md5(String value, String salt, int type) {
 		return (new Hasher()).md5(salt(value, salt, type));
 	}
@@ -37,30 +32,29 @@ public class SaltyHasher {
 	
 	private String insertMiddle(String a, String b) {
 		int index = a.length() / 2;
-	    String aBegin = a.substring(0,index);
-	    String aEnd = a.substring(index);
-	    return aBegin + b + aEnd;
+		String aBegin = a.substring(0,index);
+		String aEnd = a.substring(index);
+		return aBegin + b + aEnd;
 	}
 
 	// https://stackoverflow.com/questions/5208163/string-alternating-letters
 	private String interlaceStrings(String a, String b) {
-	    if (a == null) a = "";
-	    if (b == null) b = "";
+		if (a == null) a = "";
+		if (b == null) b = "";
 
-	    char[] chars = new char[a.length() + b.length()];
-	    int index = 0, ia = 0, ib = 0;
-	    while (ia<a.length() && ib<b.length()) {
-	        chars[index++] = a.charAt(ia++);
-	        chars[index++] = b.charAt(ib++);
-	    }
-	    while (ia<a.length()) {
-	        chars[index++] = a.charAt(ia++);
-	    }
-	    while (ib<b.length()) {
-	        chars[index++] = b.charAt(ib++);
-	    }
-
-	    return new String(chars);
+		char[] chars = new char[a.length() + b.length()];
+		int index = 0, ia = 0, ib = 0;
+		while (ia<a.length() && ib<b.length()) {
+			chars[index++] = a.charAt(ia++);
+			chars[index++] = b.charAt(ib++);
+		}
+		while (ia<a.length()) {
+			chars[index++] = a.charAt(ia++);
+		}
+		while (ib<b.length()) {
+			chars[index++] = b.charAt(ib++);
+		}
+		return new String(chars);
 	}
-	
+
 }
